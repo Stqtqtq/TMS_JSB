1cd8f512989f03b02010f3561e66599091de0726
Stqtqtq <tianquan95@gmail.com>
2024-11-20 15:59:40 +0800

    initial commit

---
+++ src/main/java/com/tms/assignment_jsb/AssignmentJsbApplication.java
@@ -0,0 +1,13 @@
+package com.tms.assignment_jsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentJsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentJsbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tms/assignment_jsb/AssignmentJsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.assignment_jsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentJsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
